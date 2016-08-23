485030e4ab3c84c7e9a2328d52ee3c5fca82997e
ferbarragan <ferbarragan.developer@gmail.com>
2016-08-23 18:59:08 -0500

    Initial Commit

---
+++ GastoHormiga/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GastoHormiga
+//
+//  Created by Christian Barragan on 23/08/16.
+//  Copyright © 2016 Christian Barragan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GastoHormiga/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GastoHormiga
+//
+//  Created by Christian Barragan on 23/08/16.
+//  Copyright © 2016 Christian Barragan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
